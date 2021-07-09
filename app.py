7d9ce9cd62b5c18ef5f6506f2729cd0be77e5d2d
Lee-Roy King <leeroywking@gmail.com>
2021-07-08 20:40:14 -0500

    hype route added

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/math/<hype>")
+def hype(hype):
+    return f"<h1>You are the best {hype}</h1>"
